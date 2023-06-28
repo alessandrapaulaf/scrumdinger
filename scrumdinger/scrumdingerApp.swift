da9f2fd22d1d12851e347950370a951217e423b7
alessandrapaulaf <alessandra.paulafrancisco@gmail.com>
2023-06-28 18:51:02 -0300

    add new project

---
+++ scrumdinger/scrumdingerApp.swift
@@ -0,0 +1,17 @@
+//
+//  scrumdingerApp.swift
+//  scrumdinger
+//
+//  Created by Alessandra on 28/06/23.
+//
+
+import SwiftUI
+
+@main
+struct scrumdingerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
